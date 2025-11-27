aff07e97b86365d285373c341cf8cc3ed3a02453
PH65133 <lapvu2007@gmail.com>
2025-11-27 11:37:03 +0700

    bài 1 lab6 COM108

--- 25FA-BL2_COM108-WD21308-PH65133/Program.c
+++ 25FA-BL2_COM108-WD21308-PH65133/Program.c
@@ -5,7 +5,30 @@
 
 int main()
 {
-    printf("Hi there!\n");
+	int n, i;
+	float tong = 0;
+	int dem = 0;
+	float trungBinh;
+	printf("Nhap so phan tu cua mang: ");
+	scanf("%d", &n);
+	int arr[100];
+	printf("Nhap cac phan tu cua mang:\n");
+	for (i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (i = 0; i < n; i++) {
+		if (arr[i] % 3 == 0) {
+			tong += arr[i];
+			dem++;
+		}
+	}
+	if (dem > 0) {
+		trungBinh = tong / dem;
+		printf("Trung binh tong cac so chia het cho 3 trong mang la: %.2f\n", trungBinh);
+	}
+	else {
+		printf("Khong co so nao chia het cho 3 trong mang.\n");
+	}
 }
 
 // Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
